f5d4f90657880c466951b812398172eeb98fa8c9
grant24 <36163487+grant24@users.noreply.github.com>
2019-10-28 18:52:25 -0400

    base code

--- ROARB/src/main.cpp
+++ ROARB/src/main.cpp
@@ -27,40 +27,11 @@ motor left_back = motor(PORT8, 0);
 // user-control mode
 int main() {
    
-    // controller code
-    int count = 0;
-    bool drive_style = 1;
-    while (true) {
-        Brain.Screen.printAt( 10, 50, "engage_electricity => %d", count++ );
+    while (true) { 
+        Brain.Screen.printAt( 10, 50, "engage_electricity => %d", Brain.timer(sec)); 
+        
+        // auton tasks
 
-        // if button is pressed, toggle driving mode => X
-        if (cntrlr.ButtonX.pressing()) {
-          drive_style = !drive_style;
-        }        
-
-        if (drive_style) {
-          // arcade style drive style
-          right_front.spin(directionType::fwd, (cntrlr.Axis3.value() - cntrlr.Axis1.value()), velocityUnits::pct);
-          right_mid.spin(directionType::fwd, (cntrlr.Axis3.value() - cntrlr.Axis1.value()), velocityUnits::pct);
-          right_back.spin(directionType::fwd, (cntrlr.Axis3.value() - cntrlr.Axis1.value()), velocityUnits::pct);
-          left_front.spin(directionType::fwd, (cntrlr.Axis3.value() + cntrlr.Axis1.value()), velocityUnits::pct);
-          left_mid.spin(directionType::fwd, (cntrlr.Axis3.value() + cntrlr.Axis1.value()), velocityUnits::pct);
-          left_back.spin(directionType::fwd, (cntrlr.Axis3.value() + cntrlr.Axis1.value()), velocityUnits::pct);
-        }
-        else {
-          // tank mode drive style
-          right_front.spin(directionType::fwd, cntrlr.Axis2.value(), velocityUnits::pct);
-          right_mid.spin(directionType::fwd, cntrlr.Axis2.value(), velocityUnits::pct);
-          right_back.spin(directionType::fwd, cntrlr.Axis2.value(), velocityUnits::pct);
-          left_front.spin(directionType::fwd, cntrlr.Axis3.value(), velocityUnits::pct);
-          left_mid.spin(directionType::fwd, cntrlr.Axis3.value(), velocityUnits::pct);
-          left_back.spin(directionType::fwd, cntrlr.Axis3.value(), velocityUnits::pct);
-        }
-
-        // hold button to turn flapper gear =? R2
-
-
-        // Allow other tasks to run
         this_thread::sleep_for(10);
     }
 
