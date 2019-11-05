19a870646362ece54bda377066caf9341b47488d
willlenore <william.lenore@gmail.com>
2019-11-04 19:27:46 -0500

    Created jerk motion in autonomous section

--- ROARB/src/main.cpp
+++ ROARB/src/main.cpp
@@ -23,17 +23,31 @@ motor right_back  = motor(PORT3, 1);
 motor left_front = motor(PORT6, 0);
 motor left_mid = motor(PORT7, 0);
 motor left_back = motor(PORT8, 0);
-
+motor lift_motor = motor(PORT10, 0);
 // user-control mode
 int main() {
    
     // timer instance
     double initial_time = Brain.timer(sec);
 
-    while (Brain.timer(sec) - initial_time <= 45) { 
+    while (Brain.timer(sec) - initial_time <= 45) {     //45 sec autonomous timer
         Brain.Screen.printAt( 10, 50, "engage_electricity => %d", Brain.timer(sec)); 
         printf("time => %f\n", Brain.timer(sec) - initial_time);
         // auton tasks
+        lift_motor.startRotateFor(1.0, deg);    //start rotating motor for lift arm - might need to switch to function call that uses velocity too
+        right_front.startRotateFor(fwd, 1.0, deg);        //concurrently start rotating all wheels
+        right_mid.startRotateFor(fwd, 1.0, deg);         
+        right_back.startRotateFor(fwd, 1.0, deg);
+        left_front.startRotateFor(fwd, 1.0, deg);
+        left_mid.startRotateFor(fwd, 1.0, deg);
+        left_back.startRotateFor(fwd, 1.0, deg);
+        
+        right_front.startRotateFor(directionType::rev, 1.0, deg);  //concurrently start rotating all wheels in reverse
+        right_mid.startRotateFor(directionType::rev, 1.0, deg);        
+        right_back.startRotateFor(directionType::rev, 1.0, deg);
+        left_front.startRotateFor(directionType::rev, 1.0, deg);
+        left_mid.startRotateFor(directionType::rev, 1.0, deg); 
+        left_back.startRotateFor(directionType::rev, 1.0, deg);
 
         this_thread::sleep_for(10);
     }
