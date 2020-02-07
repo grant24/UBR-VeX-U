e83e1f9fd3ac26b8725ccb24761e8d53f11b84a4
grant24 <36163487+grant24@users.noreply.github.com>
2020-02-06 21:32:06 -0500

    stuff

--- RTUC-2020-02-07T07-06-22/src/main.cpp
+++ RTUC-2020-02-07T07-06-22/src/main.cpp
@@ -11,17 +11,14 @@
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+vex::brain Brain;
 vex::controller cntrlr;
 
 // define your global instances of motors and other devices here
 motor main_motor_left = motor(PORT1, 0);
 motor main_motor_right = motor(PORT2, 1);
-
 motor claw_angle = motor(PORT3, 0);
-
 motor claw_left = motor(PORT4, 0);
-motor claw_right = motor(PORT5, 1);
 
 int main() {
     int count = 0;
@@ -32,6 +29,8 @@ int main() {
 
         main_motor_left.spin(fwd, cntrlr.Axis2.value(), pct);
         main_motor_right.spin(fwd, cntrlr.Axis2.value(), pct);
+
+        claw_angle.spin(fwd, cntrlr.Axis3.value(), pct);
 
         this_thread::sleep_for(10);
     }
